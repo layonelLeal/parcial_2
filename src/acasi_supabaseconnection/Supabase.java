4ac77f73b886ded10e85be9c3dbcf20abb253cad
layonel <layonelcamilolealdiaz@gmail.com>
2024-03-27 11:36:33 -0500

    reu1

--- src/acasi_supabaseconnection/Supabase.java
+++ src/acasi_supabaseconnection/Supabase.java
@@ -6,19 +6,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  *
  * @author layonel
  */
 class Supabase {
-    //https://rrkotxwnjddvzolduptl.supabase.co/auth/v1/signup
-    //anon: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJya290eHduamRkdnpvbGR1cHRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEzNzU3NTMsImV4cCI6MjAyNjk1MTc1M30.f_wch4K52M85qijPY3ovSynCUoH2ax9eyknWnIJDmuQ
-    //"Content-Type: application/json" \
-    //-d '{
-        //"email": "someone@email.com",
-        //"password": "weDWCAyNDtqrSrhuPhQu"
-    //}'
+
     public Supabase(String urlLogin, String anonkey){
         try {
             // Define la URL a la que deseas enviar la solicitud POST
@@ -26,28 +22,41 @@ class Supabase {
 
             // Datos que deseas enviar en el cuerpo de la solicitud POST (en formato JSON)
             String postData = "{\"email\": \"lleald@unal.edu.co\", \"password\": \"AdminACASI\"}";
+            //crear una clase de tipo Body(Map) {"clave": valor}
+            // body.toString();
+            
+            //definir una variable de string = "{"
+            //recorrer el mapa, obtener la clave y el valor
+            //string += "\"" + clave + "\" : \"" + valor;  
 
             // Abre una conexión HTTP
             URL url = new URL(urlSupabase);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
-            connection.setDoOutput(true);
+            connection.setDoOutput(true); //permite leer y modificar el body
 
             // Configura las propiedades de la solicitud
             connection.setRequestProperty("Content-Type", "application/json");
             connection.setRequestProperty("apiKey", anonkey);
-            connection.setRequestProperty("Content-Length", Integer.toString(postData.length()));
+            //connection.setRequestProperty("Content-Length", Integer.toString(postData.length()));
 
             // Escribe los datos en el flujo de salida de la conexión
-            try (DataOutputStream dos = new DataOutputStream(connection.getOutputStream())) {
-                dos.writeBytes(postData);
-            }
+            connection.getOutputStream().write(postData.getBytes()); //guardar información al body      
 
             // Lee la respuesta del servidor
             try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
                 String line;
                 while ((line = br.readLine()) != null) {
                     System.out.println(line);
+                    
+                    //Revisar despues
+                    //try {
+                        //JSONObject object = new JSONObject();
+                        //System.out.println("-----------------------");
+                      //  System.out.println(object.get("access_token"));
+                    //} catch (JSONException e) {
+                      //  System.out.println("No se pudo volver un json." + e.getMessage());
+                    //}
                 }
             }
         } catch (IOException e) {
