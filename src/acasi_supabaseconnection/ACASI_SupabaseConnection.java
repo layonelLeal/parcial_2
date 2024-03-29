9b0fde4c6bb84e48273a685c6b37dd594e8c8b61
layonel <layonelcamilolealdiaz@gmail.com>
2024-03-29 11:50:50 -0500

    merge Body

--- src/acasi_supabaseconnection/ACASI_SupabaseConnection.java
+++ src/acasi_supabaseconnection/ACASI_SupabaseConnection.java
@@ -14,6 +14,8 @@ public class ACASI_SupabaseConnection {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Params param = new Params(Map {grant_type = password});
+        //param.toString(); -> "?grant_type=password
         Supabase sup = new Supabase("https://rrkotxwnjddvzolduptl.supabase.co/auth/v1/token?grant_type=password",
                 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJya290eHduamRkdnpvbGR1cHRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEzNzU3NTMsImV4cCI6MjAyNjk1MTc1M30.f_wch4K52M85qijPY3ovSynCUoH2ax9eyknWnIJDmuQ");
         
